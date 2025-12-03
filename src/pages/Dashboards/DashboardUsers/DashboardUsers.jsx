e2f3aa0a2e52e54a30e564f766f4156679459117
Jonatasvm <109855262+Jonatasvm@users.noreply.github.com>
2025-12-02 22:46:52 -0400

    aaaaa

--- src/pages/Dashboards/DashboardUsers/DashboardUsers.jsx
+++ src/pages/Dashboards/DashboardUsers/DashboardUsers.jsx
@@ -3,7 +3,7 @@ import { Toaster, toast } from "react-hot-toast";
 import { ObrasManager } from "./ObrasManager"; // Ajuste o caminho se necessário
 import { UserManager } from "./UsersManager"; // Ajuste o caminho se necessário
 
-const API_IP = "http://127.0.0.1:5631";
+const API_IP = "http://91.98.132.210:5631";
 
 export default function DashboardUsers() {
   const [obrasList, setObrasList] = useState([]);
