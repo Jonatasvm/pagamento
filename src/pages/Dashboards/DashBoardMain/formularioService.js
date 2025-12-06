ad6e06a618e6374cd9bee918c28b25d7dea1a885
Jonatasvm <109855262+Jonatasvm@users.noreply.github.com>
2025-12-06 18:18:51 -0400

    Update formularioService.js

--- src/pages/Dashboards/DashBoardMain/formularioService.js
+++ src/pages/Dashboards/DashBoardMain/formularioService.js
@@ -91,7 +91,7 @@ export const deletarFormulario = async (id) => {
 
 export const criarFormulario = async (data) => {
   const payload = adapterFrontendToBackend(data);
-  payload.lancado = false;
+  payload.lancado = 0;
   const response = await api.post("/formulario", payload);
   return response.data;
 };
